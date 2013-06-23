71158249a2024d44ed84cafd9dc72249acc090cb
Thomas <pi@raspberrypi.(none)>
2013-06-23 19:57:10 +0200

    added request output, sending whole reading

--- linux/src/energycam/energycam.c
+++ linux/src/energycam/energycam.c
@@ -465,6 +465,7 @@ void sendDataToServer(char ip[], char port[], char auth[], int OCRData, int Data
 	sprintf(request,"POST /meterValue?date=%04d-%02d-%02dT%02d:%02d:%02d.000-%02d:%02d&value=%d.%d HTTP/1.0\r\nHost: %s\r\nAuthorization: Basic %s\r\n\r\n", tm.tm_year + 1900, tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec, OffsetHours, OffsetMin, OCRData, Data, ip, auth);
 	send(sock,request,strlen(request),0);
 	printf(request);
+	close(sock);
 }
 //Projektwerkstatt
 
